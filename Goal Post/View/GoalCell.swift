ef9a76435001af48a1bd47b734d2969d6ff76c16
Clement M <massardc@users.noreply.github.com>
2017-11-16 22:37:32 +0100

    Built tableView and GoalCell

---
+++ Goal Post/View/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  Goal Post
+//
+//  Created by ClementM on 16/11/2017.
+//  Copyright © 2017 ClementM. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, goalType: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = goalType
+        self.goalProgressLbl.text = String(goalProgressAmount)
+    }
+    
+}
