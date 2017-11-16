34ee5b126a80b954617d885b0974d4e8e62745a5
Clement M <massardc@users.noreply.github.com>
2017-11-16 21:53:36 +0100

    Built GoalVC

--- Goal Post/Controller/GoalVC.swift
+++ Goal Post/Controller/GoalVC.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
+        
+    }
 
 }
 
