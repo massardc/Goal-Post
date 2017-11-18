90cb888d2fdbd92b628013035212367ba5808462
Clement M <massardc@users.noreply.github.com>
2017-11-18 22:54:32 +0100

    Created CreateGoalVC class and interface

---
+++ Goal Post/Controller/CreateGoalVC.swift
@@ -0,0 +1,38 @@
+//
+//  CreateGoalVC.swift
+//  Goal Post
+//
+//  Created by ClementM on 18/11/2017.
+//  Copyright © 2017 ClementM. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var goalTextView: UILabel!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // Actions
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func shortTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtnPressed(_ sender: Any) {
+    }
+    
+}
