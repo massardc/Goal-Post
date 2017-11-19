436bd037178f932f558056e4008f8a5860ec288f
Clement M <massardc@users.noreply.github.com>
2017-11-19 16:15:24 +0100

    User can add progress. Updating display for completed tasks

--- Goal Post/View/GoalCell.swift
+++ Goal Post/View/GoalCell.swift
@@ -14,11 +14,18 @@ class GoalCell: UITableViewCell {
     @IBOutlet weak var goalDescriptionLbl: UILabel!
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var goalProgressLbl: UILabel!
+    @IBOutlet weak var completionView: UIView!
     
     func configureCell(goal: Goal) {
         self.goalDescriptionLbl.text = goal.goalDescription
         self.goalTypeLbl.text = goal.type
         self.goalProgressLbl.text = String(goal.progress)
+        
+        if goal.progress == goal.completionValue {
+            self.completionView.isHidden = false
+        } else {
+            self.completionView.isHidden = true
+        }
     }
     
 }
